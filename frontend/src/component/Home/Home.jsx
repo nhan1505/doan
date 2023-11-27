8118fa0d56c78cfae7019a378b772823dcaf5022
nhan1505 <nhandinhthanh23@gmail.com>
2023-11-28 01:04:48 +0700

    update

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -63,7 +63,7 @@ const Home = () => {
           <div className="container" id="container">
             {products &&
               products.map((product) => (
-                <ProductCard key={product._id} product={product} />
+                <ProductCard  key={product._id} product={product} />
               ))}
           </div>
          
